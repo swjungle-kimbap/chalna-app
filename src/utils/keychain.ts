0707a2d869fe68c917baf71f9cff4aa7ee2b8e27
eunsik-kim <gracely9901@gmail.com>
2024-06-21 14:31:45 +0900

    fix: keychain 조회 수정

--- src/utils/keychain.ts
+++ src/utils/keychain.ts
@@ -6,37 +6,39 @@ interface KeychainCredentials {
   service?: string;
 }
 
-export async function setKeychain(username:string, token:string) {
+const SERVICE_PREFIX = 'com.chalna';
+
+export async function setKeychain(username: string, password: string) {
   try {
-    await Keychain.setGenericPassword(username, token);
+    await Keychain.setGenericPassword(username, password, { service: `${SERVICE_PREFIX}.${username}` });
     console.log(`${username} stored in keychain`);
   } catch (error) {
     console.log(`Error storing ${username} token: `, error);
   }
 }
 
-export async function getKeychain(username:string): Promise<string | null> {
+export async function getKeychain(username: string): Promise<string | null> {
   try {
     const credentials: KeychainCredentials | false 
-      = await Keychain.getGenericPassword({username});
+      = await Keychain.getGenericPassword({ service: `${SERVICE_PREFIX}.${username}` });
     if (credentials) {
       console.log(`Using stored ${username} ${credentials.password}`);
       return credentials.password;
     } else {
-      console.log(`Not stored ${username} in keychian`);
+      console.log(`Not stored ${username} in keychain`);
       return null;
     }
   } catch (error) {
     console.error(`Error retrieving ${username} token:`, error);
-    return null
+    return null;
   }
 }
 
-export async function deleteKeychain(username:string): Promise<void> {
+export async function deleteKeychain(username: string): Promise<void> {
   try {
-    await Keychain.resetGenericPassword({username});
+    await Keychain.resetGenericPassword({ service: `${SERVICE_PREFIX}.${username}` });
     console.log(`${username} token deleted successfully`);
   } catch (error) {
     console.error(`Error deleting ${username} token`, error);
   }
-};
+}
