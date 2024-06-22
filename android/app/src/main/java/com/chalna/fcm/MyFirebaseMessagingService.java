8f8b0b78760b30cb51b85eb665d226f8c63bc2fb
jujulia3040@gmail.com <jujulia3040@gmail.com>
2024-06-22 14:26:07 +0900

    feat: FCM 수신 메시지 처리 클래스 백그라운드 처리 임시 구현 (#22)
    아직 포그라운드일 때 푸시 알림 수신 시 빈화면으로 이동함(?)

---
+++ android/app/src/main/java/com/chalna/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,41 @@
+package com.chalna.fcm;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // 메시지 수신 처리
+        if (remoteMessage.getNotification() != null) {
+            sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+    private void sendNotification(String title, String messageBody) {
+        String channelId = "default_channel";
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setContentTitle(title)
+                        .setContentText(messageBody)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, "Default Channel", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
