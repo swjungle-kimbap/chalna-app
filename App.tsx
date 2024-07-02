df82ed7a0506066be64a2bfe4970c4c792cb5855
kju0 <jujulia3040@gmail.com>
2024-07-02 11:11:51 +0900

    refactor: fcm kotlin 주석 적용, rn fcm init 추가 (#98)

--- App.tsx
+++ App.tsx
@@ -5,29 +5,32 @@ import { RecoilRoot } from 'recoil';
 import React, { useEffect } from 'react';
 import { DeviceEventEmitter } from 'react-native';
 import { navigate } from './src/navigation/RootNavigation';
+import { fcmService } from './src/service/FCMService';
 
 export default function App() {
   useEffect(() => {
-    // 이벤트 리스너 등록
-    const subscription = DeviceEventEmitter.addListener('FCMOpenScreen', (event) => {
-      const { screen, screenId } = event;
-      if (screen === "채팅") {
-        console.log('Joo Received screen value in App:', screen, screenId);
-        navigate("채팅", { chatRoomId: screenId });
-      } else {
-        console.log('Screen value is null or undefined');
-        navigate("로그인 성공", {
-          screen: "지도",
-          params: { NotificationId: screenId } 
-          }
-        )};
-      })
+      fcmService.initialize();
 
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => {
-      subscription.remove();
-    };
-  }, []);
+//     // 이벤트 리스너 등록
+//     const subscription = DeviceEventEmitter.addListener('FCMOpenScreen', (event) => {
+//       const { screen, screenId } = event;
+//       if (screen === "채팅") {
+//         console.log('Joo Received screen value in App:', screen, screenId);
+//         navigate("채팅", { chatRoomId: screenId });
+//       } else {
+//         console.log('Screen value is null or undefined');
+//         navigate("로그인 성공", {
+//           screen: "지도",
+//           params: { NotificationId: screenId }
+//           }
+//         )};
+//       })
+//
+//     // 컴포넌트 언마운트 시 이벤트 리스너 제거
+//     return () => {
+//       subscription.remove();
+//     };
+    }, []);
 
   return (
     <ThemeProvider theme={FontTheme}>
