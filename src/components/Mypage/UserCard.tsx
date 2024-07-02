ebf7f752a6ef21fcf4ba6b6354ad536b8890cc26
eunsik-kim <gracely9901@gmail.com>
2024-07-02 16:52:58 +0900

    feat: 프로필 수정 모달 추가

--- src/components/Mypage/UserCard.tsx
+++ src/components/Mypage/UserCard.tsx
@@ -3,48 +3,62 @@ import Button from "../../components/common/Button"
 import { StyleSheet, View, Text  } from "react-native";
 import { useRecoilValue } from "recoil";
 import { userInfoState } from "../../recoil/atoms";
+import { useState } from 'react';
+import EditModal from './EditModal';
 
 const DefaultImgUrl = '../../assets/images/anonymous.png';
 const editButtonUrl ='../../assets/buttons/EditButton.png'
 
 const UserCard = () => {
   const user = useRecoilValue(userInfoState);
+  const [username, setUsername] = useState(user.username);
+  const [statusMessage, setStatusMessage] = useState(user.message ? user.message : "상태 메세지를 입력해주세요");
+  const [showNameEditModal, setShowNameEditModal] = useState<boolean>(false);
+  const [showStatusEditModal, setShowStatusEditModal] = useState<boolean>(false);
 
   return (
+  <>
+    {showNameEditModal && (<EditModal setValue={setUsername} value={username} maxLength={10}
+      modalVisible={showNameEditModal}  closeModal={() => setShowNameEditModal(false)}/>)}
+    {showStatusEditModal && (<EditModal value={statusMessage}  setValue={setStatusMessage} maxLength={20}
+      modalVisible={showStatusEditModal} closeModal={() => setShowStatusEditModal(false)}/>)}
     <View style={styles.myProfileContainer}>
-    <View style={styles.headerText}>
-      <Text style={styles.text}>내 프로필</Text>
-    </View>
-    <View style={styles.header}>
-      <Button 
-        iconSource={user.profileImageUrl ? require(DefaultImgUrl) : require(DefaultImgUrl)} 
-        imageStyle={styles.avatar} 
-      />
-      <View>
-        <View style={styles.username}>
-          <Text style={styles.text}>{user.username}</Text>
-          <Button iconSource={require(editButtonUrl)}/>
-        </View>
-        <View style={styles.username}>
-          <Text 
-            style={[
-              styles.statusMessage,
-              { color: user.message ? '#fff' : '#979797' } 
-            ]}
-          >
-            {user.message ? "user.message" : "상태 메세지를 입력해주세요"}
-          </Text>
-          <Button iconSource={require(editButtonUrl)}/>
+      <View style={styles.headerText}>
+        <Text style={styles.text}>내 프로필</Text>
+      </View>
+      <View style={styles.header}>
+        <Button 
+          iconSource={user.profileImageUrl ? require(DefaultImgUrl) : require(DefaultImgUrl)} 
+          imageStyle={styles.avatar} 
+        />
+        <View>
+          <View style={styles.username}>
+            <Text style={styles.text}>{username}</Text>
+            <Button iconSource={require(editButtonUrl)} 
+              onPress={() => setShowNameEditModal(true)}/>
+          </View>
+          <View style={styles.username}>
+            <Text 
+              style={[
+                styles.statusMessage,
+                { color: user.message ? '#fff' : '#979797' } 
+              ]}>
+              {statusMessage}
+            </Text>
+            <Button iconSource={require(editButtonUrl)} 
+              onPress={() => setShowStatusEditModal(true)}/>
+          </View>
         </View>
       </View>
     </View>
-  </View>
+  </>
   );
 }
 
 const styles = StyleSheet.create({
   username: {
     flexDirection: 'row',
+    width:250,
   },
   headerText: {
     alignItems: "flex-start",
