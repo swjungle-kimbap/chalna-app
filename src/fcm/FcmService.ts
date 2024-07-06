eab042b9de8bbed2b8dd5dbd202425893ca5cbeb
kju0 <jujulia3040@gmail.com>
2024-07-06 13:30:16 +0900

    feat: (이식 수술 필요) post notification 추가 (#145)

--- src/fcm/FcmService.ts
+++ src/fcm/FcmService.ts
@@ -1,13 +1,12 @@
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import PushNotification from 'react-native-push-notification';
-import { Platform } from 'react-native';
 import { fcmConfig } from './FcmConfig';
+
 
 class FCMService {
   initialize() {
     // 앱 등록
     fcmConfig.registerAppWithFCM();
+
+    fcmConfig.requestNotificationPermission();
 
     // 포그라운드, 백그라운드 핸들러 등록
     fcmConfig.setupMessageHandlers();
@@ -15,6 +14,7 @@ class FCMService {
     // fcm push 알림 설정
 		fcmConfig.configurePushNotification();
   }
+
 }
 
 export const fcmService = new FCMService();
