5605c0ee2e8f29ef86f42e06934465ed6e06e3e6
JeeAh <banglejeeah@gmail.com>
2024-07-08 23:16:51 +0900

    fix chatroomlist margin bottom

--- src/screens/Chatting/ChattingListScreen.tsx
+++ src/screens/Chatting/ChattingListScreen.tsx
@@ -159,6 +159,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
+        marginBottom:80,
     },
     loadingContainer: {
         flex: 1,
