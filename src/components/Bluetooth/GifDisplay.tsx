afc1b93503f82db179141c13cb0ca7d771e3eeab
kju0 <jujulia3040@gmail.com>
2024-07-10 23:49:58 +0900

    style: gif component 추가 (#220)

---
+++ src/components/Bluetooth/GifDisplay.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+const GifDisplay = ({ source, style }) => {
+    return (
+        <View style={styles.container}>
+            <FastImage
+                source={source}
+                style={[styles.gif, style]}
+                resizeMode={FastImage.resizeMode.contain}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    gif: {
+        width: 200,
+        height: 200,
+    },
+});
+
+export default GifDisplay;
