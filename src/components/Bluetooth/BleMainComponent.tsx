a714b050a6169b34c14fadd7f5ade344f59ca561
kju0 <jujulia3040@gmail.com>
2024-07-13 23:37:41 +0900

    refactor: 애니메이션 변수명 변경 (#242)

--- src/components/Bluetooth/BleMainComponent.tsx
+++ src/components/Bluetooth/BleMainComponent.tsx
@@ -12,7 +12,7 @@ interface BleMainComponentProps {
 
 const BleMainComponent: React.FC<BleMainComponentProps> = ({ isKeyboardVisible, uuids, style }) => {
   const animationRef = useRef<LottieView>(null);
-  const fadeAnim2 = useRef(new Animated.Value(1)).current;
+  const fadeAnim = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
     if (!isKeyboardVisible && animationRef.current) {
@@ -22,13 +22,13 @@ const BleMainComponent: React.FC<BleMainComponentProps> = ({ isKeyboardVisible, 
 
   useEffect(() => {
     if (uuids.size === 0) {
-      Animated.timing(fadeAnim2, {
+      Animated.timing(fadeAnim, {
         toValue: 0,
         duration: 4000,
         useNativeDriver: true,
       }).start();
     } else {
-      Animated.timing(fadeAnim2, {
+      Animated.timing(fadeAnim, {
         toValue: 1,
         duration: 500,
         useNativeDriver: true,
@@ -38,14 +38,14 @@ const BleMainComponent: React.FC<BleMainComponentProps> = ({ isKeyboardVisible, 
 
   return (
     <SafeAreaView style={[styles.bleMainContainer, style]}>
-      <Animated.View style={[styles.detectContainer, {opacity: fadeAnim2 }]}>
+      <Animated.View style={[styles.detectContainer, {opacity: fadeAnim }]}>
         <DetectDisplay uuids={uuids} />
       </Animated.View>
       <LottieView
         ref={animationRef}
         source={require('../../assets/animations/WaveAnimation.json')}
         style={styles.lottieImage}
-        speed={2.5}
+        speed={1.5}
         loop
         onLayout={() => {
           if (!isKeyboardVisible && animationRef.current) {
