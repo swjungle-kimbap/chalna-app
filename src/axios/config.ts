bbb64cb13de801022bd31c0de77ff8d001f3beb4
eunsik-kim <gracely9901@gmail.com>
2024-07-13 20:05:11 +0900

    fix: 친구 목록 채팅방 이동 api 연결 수정

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -44,7 +44,7 @@ export const urls = {
     CHATROOM_LIST_URL: getFullUrl(Config.CHATROOM_PATH),
     CHATROOM_MSG_URL: getFullUrl(Config.CHATROOM_CHAT_MSG_PATH),
     CHATROOM_LEAVE_URL: getFullUrl(Config.CHATROOM_LEAVE_PATH),
-    CHATROOM_JOIN_URL: getFullUrl(Config.CHATROOM_JOIN_PATH),
+    CHATROOM_JOIN_URL: getFullUrl(Config.LOCAL_CHAT_PATH),
 
     SET_CUR_POS_URL: getFullUrl(Config.SET_CUR_POS_PATH),
 
@@ -54,6 +54,7 @@ export const urls = {
     JOIN_LOCAL_CHAT_URL: getFullUrl(Config.LOCAL_CHAT_SET_PATH),
 
     FILE_UPLOAD_URL: getFullUrl(Config.FILE_UPLOAD_PATH),
-    FILE_DOWNLOAD_URL: getFullUrl(Config.FILE_DOWNLOAD_PATH)
+    FILE_DOWNLOAD_URL: getFullUrl(Config.FILE_DOWNLOAD_PATH),
 
+    GET_FRIEND_JOINT_URL: getFullUrl(Config.GET_FRIEND_JOINT_PATH),
 };
