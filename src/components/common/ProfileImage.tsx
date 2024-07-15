f1d6bf3211b4a6e10ac1c0c83e02529c250418a9
JeeAh <banglejeeah@gmail.com>
2024-07-15 23:58:06 +0900

    added useMemo in profileImage component

--- src/components/common/ProfileImage.tsx
+++ src/components/common/ProfileImage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import FastImage, { Source } from "react-native-fast-image";
 import { Image } from "react-native";
 import { getImageUri } from "../../utils/FileHandling";
@@ -8,32 +8,28 @@ const defaultImg = require('../../assets/images/anonymous.png');
 const ProfileImage = ({ profileImageId, avatarStyle }) => {
   const [profileUri, setProfileUri] = useState("");
 
-  useEffect(() => {
-    const getUri = async () => {
-      try {
-        const savedUri = await getImageUri(profileImageId);
-        setProfileUri(savedUri);
-      } catch (error) {
-        console.error("Failed to get profile image URL", error);
-        setProfileUri("");
-      }
-    };
-
-    getUri();
+  const getUri = useCallback(async () => {
+    try {
+      const savedUri = await getImageUri(profileImageId);
+      setProfileUri(savedUri);
+    } catch (error) {
+      console.error("Failed to get profile image URL", error);
+      setProfileUri("");  // or setProfileUri(defaultImg) if you want to use default image URI
+    }
   }, [profileImageId]);
 
+  useEffect(() => {
+    getUri();
+  }, [getUri]);
+
+  const imageSource = useMemo(() => profileUri ? { uri: profileUri, priority: FastImage.priority.normal } : defaultImg, [profileUri]);
+
   return (
-    <>
-      {profileUri ? (
-        <FastImage
+      <FastImage
           style={avatarStyle}
-          source={{ uri: profileUri, priority: FastImage.priority.normal } as Source}
+          source={imageSource as Source}
           resizeMode={FastImage.resizeMode.cover}
-        />
-      ) : (
-        <Image source={defaultImg} style={avatarStyle} />
-      )}
-    </>
+      />
   );
 };
 
